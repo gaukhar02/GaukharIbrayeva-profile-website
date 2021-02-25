c9ce148b25329b9fb3a0822eae41d3d26825129c
gaukhar02 <190103296@stu.sdu.edu.kz>
2021-02-26 00:00:59 +0600

    post

--- gitProject/routes/web.php
+++ gitProject/routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
 
+use App\Models\Client;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -19,3 +21,30 @@ Route::get('/', function () {
 Route::get('/Laravel', function () {
     return view('index');
 })-> name('Laravel');
+
+
+Route::get('client/add', function () {
+     DB:: table('clients')-> insert([
+          'name' => 'Gaukhar',
+          'surname' => 'Ibrayeva',
+          'age' => '18',
+     ]);
+ });
+
+Route::get('client', function () {
+     $client = Client::find(1);
+     return $client;
+});
+
+Route::get('post/create', function () {
+     DB:: table('post')-> insert([
+          'title' => 'Salutation',
+          'body' => 'Hello, welcome, this is inside my database. Here you can write about anything. Just be sure to include the title of your post.',
+
+     ]);
+ });
+
+Route::get('post', function () {
+     $post = post::find(1);
+     return $post;
+});
